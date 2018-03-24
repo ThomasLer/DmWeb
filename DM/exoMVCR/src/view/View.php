085ce9b87128d99aa78fd3a01a0c993435382231
asurion <asurion61@gmail.com>
2018-03-24 16:53:52 +0100

    HTML5 validator du truc de merde Script de merde

--- DM/exoMVCR/src/view/View.php
+++ DM/exoMVCR/src/view/View.php
@@ -245,10 +245,7 @@ class View
                 </main>
             </div>
         </body>
-        <script
-                src="https://code.jquery.com/jquery-3.1.1.min.js"
-                integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
-                crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
         <script src="skin/semantic.min.js"></script>
         </html>
 
